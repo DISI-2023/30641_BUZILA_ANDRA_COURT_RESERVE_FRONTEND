9af667e07dac39ca06474a95502f68b489365bd6
dudlarisa <larisadud01@gmail.com>
2023-04-22 14:51:23 +0300

    #46 - Update App component to include menu for all routes and modify gome style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import Menu from "./components/Menu.js";
+import { Route, Routes } from "react-router-dom";
+import ApplicationRoutes from "./AppRoutes";
+import { useMemo } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const appRoutes = useMemo(() => {
+    return ApplicationRoutes();
+  }, []);
+
+  return (
+    <div className="App">
+      <Menu />
+      <Routes>
+        {appRoutes.map((route, index) => {
+          const { element, requireAuth, ...rest } = route;
+          return <Route key={index} {...rest} element={element} />;
+        })}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
