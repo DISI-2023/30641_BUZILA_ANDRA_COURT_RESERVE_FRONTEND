515568eb2fa90350ebfc34253d538751981383e3
Alexandra Pop <alexandra.pop201@gmail.com>
2023-04-22 11:54:32 +0300

    #29-F-Added reservations page api

--- src/pages/reservations/reservations-page.js
+++ src/pages/reservations/reservations-page.js
@@ -1,35 +1,76 @@
 import SimpleTable from "../../components/table/simple-table";
+import * as ReservationsAPI from "../../api/reservations-api";
+import React, {useEffect, useState} from "react";
+import ErrorHandler from "../../commons/errorhandling/error-handler";
 
-const data = [
-    { col1: 'Minsk', col2: '27', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'Vilnius', col2: '30', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'London', col2: '23', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'Vilnius', col2: '30', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'London', col2: '23', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'Minsk', col2: '27', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'Vilnius', col2: '30', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'London', col2: '23', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'Minsk', col2: '27', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'Vilnius', col2: '30', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'London', col2: '23', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'Minsk', col2: '27', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'Vilnius', col2: '30', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'London', col2: '23', col3: 'rain', col4: 'bla', col5: 'tra' },
-    { col1: 'Minsk', col2: '27', col3: 'rain', col4: 'bla', col5: 'tra' },
+const dummyData = [
+    {
+        locationId: '1',
+        locationAddress: 'Cluj-Napoca',
+        locationLongitude: 'sadasdsadasd',
+        locationLatitude: 'dvzczcxzxc',
+        courtId: '1',
+        courtType: 'ceva',
+        courtName: 'altceva'
+    },
+    {
+        locationId: '1',
+        locationAddress: 'Cluj-Napoca',
+        locationLongitude: 'sadasdsadasd',
+        locationLatitude: 'dvzczcxzxc',
+        courtId: '1',
+        courtType: 'ceva',
+        courtName: 'altceva'
+    },
 ];
 
-const columns = [
-        { Header: 'City', accessor: 'col1', },
-        { Header: 'Temperature', accessor: 'col2', },
-        { Header: 'Weather Forecast', accessor: 'col3', },
-        { Header: 'Weather Forecast', accessor: 'col4', },
-        { Header: 'Weather Forecast', accessor: 'col5', },
+const courtsColumns = [
+    { Header: 'Location ID', accessor: "locationId" },
+    { Header: 'Location Address', accessor: "locationAddress" },
+    { Header: 'Location Longitude', accessor: "locationLongitude" },
+    { Header: 'Location Latitude', accessor: "locationLatitude" },
+    { Header: 'Court ID', accessor: "courtId" },
+    { Header: 'Court Type', accessor: "courtType" },
+    { Header: 'Court Name', accessor: "courtName" },
 ];
 
 function ReservationsPage(){
+
+    const[data, setData] = useState([]);
+    const [columns, setColumns] = useState(courtsColumns);
+    const[error, setError] = useState(0);
+
+    useEffect(() => {
+        //fetchAllLocationsWithCourts();
+    }, []);
+
+    function fetchAvailableCourts(searchInput) {
+        return ReservationsAPI.getAvailableCourts(searchInput, (result, status) => {
+            if (result !== null && status === 200) {
+                setData(result);
+            } else {
+                setError(status);
+            }
+        });
+    }
+
+    function fetchAllLocationsWithCourts(){
+        return ReservationsAPI.getAllLocationsWithCourts((result, status) => {
+            if (result != null && status == 200){
+                setData(result);
+            }
+            else{
+                setError(status);
+            }
+        })
+    }
+
     return(
         <div>
-            <SimpleTable data={data} columns={columns}/>
+            <SimpleTable data={dummyData} columns={columns}/>
+            {
+                error > 0 && <ErrorHandler />
+            }
         </div>
     );
 }
