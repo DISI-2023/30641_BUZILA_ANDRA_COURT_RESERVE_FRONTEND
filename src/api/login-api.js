8724b6b61fff40dc3d1136df89793128b0086b0c
Alexandra Pop <alexandra.pop201@gmail.com>
2023-05-04 14:10:16 +0300

    #42-F-Created reset password api and form for getting the email

--- src/api/login-api.js
+++ src/api/login-api.js
@@ -1,22 +1,20 @@
-import { HOST } from '../commons/hosts';
+import { HOST } from "../commons/hosts";
 import RestApiClient from "../commons/api/rest-client";
 
 const endpoint = {
-    login: '/login'
+  login: "/login",
 };
 
 function loginUser(user, callback) {
-    let request = new Request(HOST.backend_api + endpoint.login, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    });
-    
-    RestApiClient.performRequest(request, callback);
+  let request = new Request(HOST.backend_api + endpoint.login, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  RestApiClient.performRequest(request, callback);
 }
-export {
-    loginUser,
-}
+export { loginUser };
---
+++ src/api/reset-password-api.js
@@ -0,0 +1,39 @@
+import { HOST } from "../commons/hosts";
+import RestApiClient from "../commons/api/rest-client";
+
+const endpoint = {
+  resetPassword: "/resetPassword",
+};
+
+function getResetPasswordEmail(email, callback) {
+  let request = new Request(
+    HOST.backend_api + endpoint.resetPassword + "/changePassword",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(email),
+    }
+  );
+
+  RestApiClient.performRequest(request, callback);
+}
+
+function resetPassword(data, callback) {
+  let request = new Request(
+    HOST.backend_api + endpoint.resetPassword + "/resetPasswordUser",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
+  );
+
+  RestApiClient.performRequest(request, callback);
+}
+export { getResetPasswordEmail, resetPassword };
